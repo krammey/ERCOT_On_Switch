ced865669d28b6a86de4fb6bea9d135474ccba4c
krammey <k.ramirezmeyers@gmail.com>
2019-06-11 12:13:34 -0500

    commented .R

--- ERCOT_2017/ERCOT_2017_Cleaning.R
+++ ERCOT_2017/ERCOT_2017_Cleaning.R
@@ -2,8 +2,8 @@
 
 # Author: Katrina Ramirez-Meyers
 # Version control information available on Github
-# This code was written to import 2017 powerplant and load data into 12* hourly models
-# *one for each month
+# This code was written to import 2017 powerplant and load data into 12* hourly models. -> *one for each month
+# Last updated 06/11/2019
 
 
 # Define useful paths
@@ -40,83 +40,74 @@ Z4_data <- Z4_data[1:tp,]
 PLEXOS_zone_data <- cbind.data.frame(Z1_data, Z2_data[5], Z3_data[5], Z4_data[5])
 # PLEXOS_zone_data$Panhandle <- 0
 
-###############
+##########################################################################################
+#             Create January files only - will be copied later
+##########################################################################################
 SaveTo <- "./2017_01/inputs/" # Save to January model for now
 
 
-############################ These reference FCe data in '../Fce_R_Development/20190405_Fce-To-SWITCH.R/
-
 # generation_projects_info.tab --------------------------------------------
-# FCeGenInfoTab = paste(c(AnnualModel2015,"generation_projects_info.tab"), collapse = "")
-# fce_gen_info <- read.delim(file = FCeGenInfoTab, header = T, sep = "\t")
-# View(rbind.data.frame(lapply(fce_gen_info, class),lapply(ercot_gen_info, class)))
+# Create empty data frame
 ercot_gen_info <- data.frame(matrix(ncol = 10, nrow = dim(ercot_data)[1]))
 colnames(ercot_gen_info) <- c("GENERATION_PROJECT","gen_tech","gen_load_zone","gen_connect_cost_per_mw","gen_variable_om","gen_max_age","gen_is_variable","gen_is_baseload","gen_energy_source","gen_full_load_heat_rate")
 
-
+# Fill in data
 ercot_gen_info$GENERATION_PROJECT <- as.factor(gsub(' ','_',ercot_data$Name)) #Replace spaces with underscores
 ercot_gen_info$gen_tech <- as.factor(gsub(' ','_',ercot_data$Tech))
-ercot_gen_info$gen_load_zone <- as.factor(ercot_data$Zone)
-# For now, I'm deleting everything with zone entry that's empty or is Panhandle
-
+ercot_gen_info$gen_load_zone <- as.factor(ercot_data$Zone) # For now, I'm deleting everything with zone entry that's empty or is Panhandle
 ercot_gen_info$gen_connect_cost_per_mw <- as.integer(0)
-ercot_gen_info$gen_variable_om <- 0#ercot_data$VOM.MWh # Need to make sure the units are right # Round(GENERIC_VOM_Cost_USD_per_MWh / (1 + GENERIC_CPI_Inflation_Rate_2010-2011))
+ercot_gen_info$gen_variable_om <- 0 # Can update this using formula: Round(GENERIC_VOM_Cost_USD_per_MWh / (1 + GENERIC_CPI_Inflation_Rate_2010-2011))
 ercot_gen_info$gen_max_age <- as.integer(100)
-ercot_gen_info$gen_is_variable <- as.integer(0) # need to add consolidated renewables back in
+ercot_gen_info$gen_is_variable <- as.integer(0) # note there are no renewables in this scenario
 ercot_gen_info$gen_is_baseload <- as.integer(0)
-ercot_gen_info$gen_energy_source <- as.factor(ercot_data$Fuel) # where are the non-fuels in the ERCOT data?
-ercot_gen_info$gen_full_load_heat_rate <- as.factor(ercot_data$PLEXOS.AHRs..MMBtu.MWh.)
-# ercot_gen_info$GENERATION_PROJECT[is.na(ercot_gen_info$gen_full_load_heat_rate)] <- levels(ercot_gen_info$gen_full_load_heat_rate)[20]
-# The plants below were given heat rates as similar plants nearby in the list
+ercot_gen_info$gen_energy_source <- as.factor(ercot_data$Fuel)
+ercot_gen_info$gen_full_load_heat_rate <- as.factor(ercot_data$PLEXOS.AHRs..MMBtu.MWh.) # The plants below were given same heat rates as similar plants nearby in the list:
 # [1] Ferguson_Replacement
 # [2] Panda_Sherman       
 # [3] Panda_Temple_1      
 # [4] Panda_Temple_2  
-ercot_gen_info$gen_full_load_heat_rate[ercot_gen_info$gen_full_load_heat_rate == 0] <- levels(ercot_gen_info$gen_full_load_heat_rate)[20]
+ercot_gen_info$gen_full_load_heat_rate[ercot_gen_info$gen_full_load_heat_rate == 0] <- levels(ercot_gen_info$gen_full_load_heat_rate)[20] # I can't remember why I did this part... it doesn't seem to change anything
 
-
+# Save .tab
 write.table(ercot_gen_info, paste(c(SaveTo,"generation_projects_info.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
 
 
 
 # gen_build_predetermined.tab ---------------------------------------------
-# FCePredetTab = paste(c(AnnualModel2015,"gen_build_predetermined.tab"), collapse = "")
-# fce_predet <- read.delim(file = FCePredetTab, header = T, sep = "\t")
-# View(rbind.data.frame(lapply(fce_predet, class),lapply(ercot_predet, class)))
+# Create empty data frame
 ercot_predet <- data.frame(matrix(ncol = 3, nrow = dim(ercot_data)[1]))
 colnames(ercot_predet) <- c("GENERATION_PROJECT","build_year","gen_predetermined_cap")
+# Fill in data
 ercot_predet$GENERATION_PROJECT <- ercot_gen_info$GENERATION_PROJECT
 ercot_predet$build_year <- as.integer(ercot_data$Build.Year)
-# For now setting empty years as 2017
-ercot_predet$build_year[is.na(ercot_predet$build_year)] <- as.integer(2017)
+ercot_predet$build_year[is.na(ercot_predet$build_year)] <- as.integer(2017) # For now setting empty years as 2017
 ercot_predet$gen_predetermined_cap <- ercot_data$Max.Cap.MW
+# Save .tab
 write.table(ercot_predet, paste(c(SaveTo,"gen_build_predetermined.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
 
 
 
 # gen_build_costs.tab -----------------------------------------------------
-
-# View(rbind.data.frame(lapply(fce_build, class),lapply(ercot_build, class)))
+# Create empty data frame
 ercot_build <- data.frame(matrix(ncol = 4, nrow = dim(ercot_data)[1] ))
 colnames(ercot_build) <- c("GENERATION_PROJECT","build_year","gen_overnight_cost","gen_fixed_om")
+# Fill in data
 ercot_build$GENERATION_PROJECT <- ercot_gen_info$GENERATION_PROJECT
 ercot_build$build_year <- as.integer(ercot_data$Build.Year)
-# for now setting this year as build year for those without an entry
-ercot_build$build_year[is.na(ercot_build$build_year)] <- as.integer(2017)
+ercot_build$build_year[is.na(ercot_build$build_year)] <- as.integer(2017) # for now setting setting empty years as 2017
 ercot_build$gen_overnight_cost <- as.integer(100) # test value
 ercot_build$gen_fixed_om <- 0 # test value. Actual values are in SCJ database
+# Export .tab
 write.table(ercot_build, paste(c(SaveTo,"gen_build_costs.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
 
-  ############################ These reference generation_projects_info
-
 # load_zones.tab ----------------------------------------------------------***
-# ercot_zones <- as.data.frame(unique(ercot_gen_info$gen_load_zone))
-# View(rbind.data.frame(lapply(fce_zones, class),lapply(ercot_zones, class)))
+# Create empty data frame
 ercot_zones <- data.frame(matrix(ncol = 1, nrow = 4))
 colnames(ercot_zones) <- "LOAD_ZONE"
+# Fill in data
 ercot_zones$LOAD_ZONE <- as.factor(c("Northeast", "South", "Coast", "West")) 
 write.table(ercot_zones, paste(c(SaveTo,"load_zones.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
@@ -124,61 +115,72 @@ write.table(ercot_zones, paste(c(SaveTo,"load_zones.tab"), collapse = ""), sep="
 
 
 # fuels.tab ---------------------------------------------------------------***
-# FCeFuelsTab = paste(c(AnnualModel2015,"fuels.tab"), collapse = "")
-# fce_fuels <- read.delim(file = FCeFuelsTab, header = T, sep = "\t")
-# View(rbind.data.frame(lapply(fce_fuels, class),lapply(ercot_fuels, class)))
+# Get list of unique fuels in ercot data, excluding Hydro - the only nonfuel in this data set
 fuels <- unique(ercot_gen_info$gen_energy_source[!(ercot_gen_info$gen_energy_source %in%c('Hydro'))])
+# Create empty data frame
 ercot_fuels <- data.frame(matrix(ncol = 3, nrow = length(fuels)))
 colnames(ercot_fuels) <- c("fuel","co2_intensity","upstream_co2_intensity")
+# Fill in data
 ercot_fuels$fuel <- fuels
-
+# Fill in emissions info by averaging ercot data by fuel type
 for(i in 1:dim(ercot_fuels)[1]){
   ercot_fuels$co2_intensity[i] <- mean(ercot_data$CO2.lb.MMBtu[ercot_data$Fuel == ercot_fuels$fuel[i]])/2204.62
-  ercot_fuels$upstream_co2_intensity[i] <- ercot_fuels$co2_intensity[i]/10 # this is just an estimate base on trend in fce data
+  ercot_fuels$upstream_co2_intensity[i] <- ercot_fuels$co2_intensity[i]/10 # this is just an estimate based on trend in fce data
 }
-
-
+# Export .tab
 write.table(ercot_fuels, paste(c(SaveTo,"fuels.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
 
 
 
 # periods.tab -------------------------------------------------------------UNCHANGED
-
-# AnnualPeriods = paste(c(AnnualModel2015,"periods.tab"), collapse = "")
-# fce_periods <- read.delim(file = AnnualPeriods, header = T, sep = "\t")
-# View(rbind.data.frame(lapply(fce_periods, class),lapply(periods, class)))
-
+# Create empty data frame
 periods <- data.frame(matrix(ncol = 3, nrow = 1))
 names(periods) <- c("INVESTMENT_PERIOD","period_start","period_end")
+# Fill in data
 periods[1,] <- 2017
+# Export .tab
 write.table(periods, paste(c(SaveTo,"periods.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
 
 
 
 
+
+
+
+##########################################################################################
+#       Create remaining files by copying January or creating time-dependent files
+##########################################################################################
+
 # Define useful time arrays
 num_days = c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 hours = as.character(c("00","01","02","03","04","05","06","07","08","09",10:23))
 years = 2017
-# months = as.character(c("01"))
-months = as.character(c("01","02","03","04","05","06","07","08","09",10,11,12)) # to expand the for-loops to do all twelve months, remove ))#
+# months = as.character(c("01")) # you can uncomment this and comment the line below if you just want to make a January test model
+months = as.character(c("01","02","03","04","05","06","07","08","09",10,11,12))
 days_max = as.character(c("01","02","03","04","05","06","07","08","09",10:31))
 
 
+############################ Already ran this. Doing it again will output an error
+# create directories for each month
+# for(m in 1:length(months)){
+#   SaveTo <- paste(c("./2017_",months[m],"/inputs/"), collapse = "")
+#   dir.create(paste(c("2017_",months[m]), collapse = ""))
+#   dir.create(SaveTo) 
+# }
 
 
+# Loop through months and create Switch input files
 for(m in 1:length(months)){
   
-  
-  
-  # create directories for each month
+  # define directories for each month
   SaveTo <- paste(c("./2017_",months[m],"/inputs/"), collapse = "")
-  # dir.create(paste(c("2017_",months[m]), collapse = ""))
-  # dir.create(SaveTo)############################ Already ran this. Doing it again will output an error
   print(SaveTo)
   
+  ########################################################### 
+  # These are copied because they don't change between months
+  ###########################################################
   # Copy the .tabs generated for January model above
   write.table(ercot_gen_info, paste(c(SaveTo,"generation_projects_info.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   write.table(ercot_predet, paste(c(SaveTo,"gen_build_predetermined.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
@@ -186,55 +188,48 @@ for(m in 1:length(months)){
   write.table(ercot_zones, paste(c(SaveTo,"load_zones.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   write.table(ercot_fuels, paste(c(SaveTo,"fuels.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   write.table(periods, paste(c(SaveTo,"periods.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
-
-  
-  ############################ NEED TO CONFIRM if these remain unchanged
-  # modules.txt ----------------------------------------------------------UNCHANGED
-  modules = paste(c(AnnualModel2015,"modules.txt"), collapse = "")
-  file.copy(modules, SaveTo)
-  
-  # switch_inputs_version.txt ----------------------------------------------------------UNCHANGED
-  version = paste(c(AnnualModel2015,"switch_inputs_version.txt"), collapse = "")
-  file.copy(version, SaveTo)
-  
-  # financials.dat ----------------------------------------------------------UNCHANGED
-  AnnualFinancials = paste(c(AnnualModel2015,"financials.dat"), collapse = "")
-  file.copy(AnnualFinancials, SaveTo)
+  file.copy('modules.txt', SaveTo)
+  file.copy('switch_inputs_version.txt', SaveTo)
+  file.copy('financials.dat', SaveTo)
+  file.copy("./2017_01/inputs/variable_capacity_factors.tab", SaveTo) # There are no renewables in Sam's 2017 model, so all variable capacity factor columns are empty so I'm just copying that into the other months
   
   
-  # non_fuel_energy_sources.tab ---------------------------------------------UNCHANGED
   
+  ########################################################### 
+  # These are month-dependent
+  ###########################################################
+  
+  # non_fuel_energy_sources.tab ---------------------------------------------
+  # Get list of non-fuels
   nonfuels <- unique(ercot_gen_info$gen_energy_source[!(ercot_gen_info$gen_energy_source %in% fuels )])
+  # Create empty data frame
   ercot_nonfuels <- data.frame(matrix(ncol = 1, nrow = length(nonfuels)))
   colnames(ercot_nonfuels) <- c("energy_source")
+  # Fill in data
   ercot_nonfuels$energy_source <- nonfuels
-  
-  
+  # Export .tab
   write.table(ercot_nonfuels, paste(c(SaveTo,"non_fuel_energy_sources.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
 
-  # create directories for each month
-  
   # timeseries.tab ---------------------------------------------------------- 
-  # AnnualTimeseries = paste(c(AnnualModel2015,"timeseries.tab"), collapse = "")
-  # fce_tseries <- read.delim(file = AnnualTimeseries, header = T, sep = "\t")
-  # View(rbind.data.frame(lapply(fce_tseries, class),lapply(t_series, class)))
-  
+  # Create empty data frame
   t_series <- data.frame(matrix(ncol = 5, nrow = 1))
   names(t_series) <- c("TIMESERIES","ts_period","ts_duration_of_tp","ts_num_tps","ts_scale_to_period")
-  # Unchanged columns in this .tab are:
+  # Fill in data
   t_series$ts_duration_of_tp <- as.integer(1)     # duration, in hours, of each timepoint within a timeseries
   t_series$TIMESERIES <- as.factor(paste(c("2017_",months[m]), collapse = ""))
   t_series$ts_period <- as.integer(2017)
   t_series$ts_num_tps <- as.integer(24*num_days[m]) # number of timepoints in a series
   t_series$ts_scale_to_period <- 8766/(24*num_days[m])
-  
+  # Export .tab
   write.table(t_series, paste(c(SaveTo,"timeseries.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   
   
   
   # timepoints.tab ---------------------------------------------------------- 
+  # Create empty data frame
   t_points <- data.frame(matrix(ncol = 3, nrow = num_days[m]*length(hours)))
   colnames(t_points) <- c("timepoint_id","timestamp","timeseries")
+  # Fill in data
   t_points$timeseries <- t_series$TIMESERIES
   tp_start <- 24*sum(num_days[1:m-1]) + 1 # skip all previous months
   tp_end <- tp_start + 24*num_days[m] - 1
@@ -246,14 +241,16 @@ for(m in 1:length(months)){
       t_points$timestamp[count] <- paste(c(2017, as.character(months[m]), as.character(days_max[d]), as.character(hours[h])), collapse = "")
     }
   }
+  # Export .tab
   write.table(t_points, paste(c(SaveTo,"timepoints.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   
   
   
   # loads.tab ---------------------------------------------------------------
+  # Create empty data frame
   ercot_loads <- data.frame(matrix(ncol = 3, nrow = dim(ercot_zones)[1]*dim(t_points)[1]))
   colnames(ercot_loads) <- c("LOAD_ZONE","TIMEPOINT","zone_demand_mw")
-  
+  # Fill in data
   row_count = 0
   for(k in 1:dim(ercot_zones)[1]){ # loop through load zones
     for(j in 1:dim(t_points)[1]){ # loop through timepoints
@@ -263,27 +260,9 @@ for(m in 1:length(months)){
       ercot_loads$zone_demand_mw[row_count] <- as.numeric(.2*PLEXOS_zone_data[j,k+4])
     }
   }
-
-  
   # Export .tab
   write.table(ercot_loads, paste(c(SaveTo,"loads.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
   
-  
-  # variable_capacity_factors.tab ------------------------------------------- # NEED TO SAVE ANNUAL CFS, THEN RUN LOOP
-  # ercot_var_gens <- ercot_gen_info$GENERATION_PROJECT[ercot_gen_info$gen_is_variable==1] # isolate list of variable generators
-  # ercot_cfs <- data.frame(matrix(ncol = 3, nrow = length(ercot_var_gens)*dim(t_points)[1]))
-  # colnames(ercot_cfs) <- c("GENERATION_PROJECT","timepoint","gen_max_capacity_factor")
-  # ercot_cfs$GENERATION_PROJECT <- rep(ercot_var_gens, each = dim(t_points)[1])
-  # ercot_cfs$gen_max_capacity_factor <- 1 # setting all cap at 1 for now
-  # ercot_cfs$timepoint <- as.integer( rep(1:dim(t_points)[1], length(ercot_var_gens)) )
-  # # rename columns and rows
-  # names(ercot_cfs) <- c("GENERATION_PROJECT","timepoint","gen_max_capacity_factor")
-  # row.names(ercot_cfs) <- 1:dim(ercot_cfs)[1]
-  # write.table(ercot_cfs, paste(c(SaveTo,"variable_capacity_factors.tab"), collapse = ""), sep="\t",row.names = F, quote = F)
-  
-  # All variable capacity factor columns are empty so I'm just copying that into the other months
-  file.copy("./2017_01/inputs/variable_capacity_factors.tab", SaveTo)
-
   }
 
 
